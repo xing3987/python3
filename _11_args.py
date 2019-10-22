fc74161af779125ae03dec35f53c82802f824b17
chenxing <xing3987@163.com>
2019-10-23 02:11:09 +0800

    base

--- _11_args.py
+++ _11_args.py
@@ -1,3 +1,5 @@
+import helper
+
 # 使用id()查看内存地址
 a = 1
 print("%s 的地址为：%s" % (a, id(a)))
@@ -21,6 +23,45 @@ print("%s 的地址为：%s" % (b, id(b)))
 可变类型不可以作为key,不可以使用hash算法
 """
 
-c=(1,2,3)
+c = (1, 2, 3)
 print(hash(c))
+
+
+# 设置方法的默认值，有默认值的参数放在方法末尾，不然会报错
+def print_info(name, gender=True):
+    gender_text = "man"
+    if not gender:
+        gender_text = "girl"
+    print("%s 是 %s" % (name, gender_text))
+
+
+print_info("lili")
+
+"""
+参数前面加*可以接收元组数据
+参数前面加**可以接收字典类型数据
+"""
+
+
+def test_args(num, *nums, **person):
+    print(num)
+    print(nums)
+    print(person)
+
+
+helper.print_line("*")
+test_args(1)
+helper.print_line("*")
+test_args(1, 2, 3, 4)
+helper.print_line("*")
+test_args(1, 2, 3, 4, name="tom", gender="F")
+helper.print_line("*")
+
+gl_tuple = [1, 2, 3]
+gl_dir = {"name": "tom", "gender": "F"}
+
+#直接传递元组或字典，得不到预期结果，需要拆包，前面加*（元组），**（字典）
+test_args(1, gl_tuple, gl_dir)
+helper.print_line("*")
+test_args(1, *gl_tuple, **gl_dir)
 
--- _12_global.py
+++ _12_global.py
@@ -1,22 +1,25 @@
-#全局变量在方法中默认是不能更改的，如果需要改变则使用global关键子修改
+# 全局变量在方法中默认是不能更改的，如果需要改变则使用global关键子修改
 
-num=10
+num = 10
+
 
 def demo1():
-    num=99
+    num = 99
     print(num)
+
 
 demo1()
 print(num)
 
+
 def demo2():
-    #使用global关键子声明全局变量，改变值时会改变全局变量的值
+    # 使用global关键子声明全局变量，改变值时会改变全局变量的值
     global num
-    num=99
+    num = 99
+
 
 demo2()
 print(num)
-
 
 """
 代码结构如下：
@@ -27,4 +30,26 @@ import模块
 执行代码
 """
 
-#全局变量一般 g_或gl_ 开头
+# 全局变量一般 g_或gl_ 开头
+
+
+list_demo = ["x", "y", "z"]
+
+
+# 一般情况全局变量在函数中值是不会被默认改变的，但是在函数中使用append可以改变全局变量的值，因为内存地址没有改变
+def change4(list):
+    list.append("f")
+
+
+change4(list_demo)
+print(list_demo)
+
+
+# 针对列表变量，+=会调用extend方法,不会修改引用，所以改动了全局变量
+def change5(list):
+    #list += list
+    list.extend(list)
+
+
+change5(list_demo)
+print(list_demo)
