226513fdc13b85af5c0bee934091e9292f4fa46c
chenxing <xing3987@163.com>
2019-10-31 01:37:38 +0800

    object

---
+++ helper/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="helper",  # 包名
+      version="1.0",
+      description="发布模块描述信息",
+      long_description="发布模块描述信息(完整)",
+      author="作者",
+      author_email="作者邮箱",
+      url="作者主页",
+      py_modules=["_helper"])  # 主要信息，模块内容
