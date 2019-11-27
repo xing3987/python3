4347947154095ce442217559534f3fe62e8dd98a
chenxing <xing3987@163.com>
2019-11-28 01:50:52 +0800

    plant

--- 飞机大战/plane_main.py
+++ 飞机大战/plane_main.py
@@ -1,4 +1,3 @@
-import pygame
 from 飞机大战.plane_sprites import *
 
 
@@ -9,17 +8,63 @@ class PlaneGame(object):
         print("游戏初始化")
 
         # 1 创建游戏的窗口
-        self.screen = pygame.display.set_mode((400, 700))
+        self.screen = pygame.display.set_mode(SCREEN_RECT.size)
         # 2 创建游戏的时钟
         self.clock = pygame.time.Clock()
         # 3 调用私有方法，精灵和精灵组的创建
         self.__create_sprites()
 
     def __create_sprites(self):
+        """创建精灵和精灵组"""
+
+        # 1.创建背景精灵和精灵组
+        # bg1 = Background("./images/background.png")
+        bg1 = Background()
+        # 定义第二个背景精灵，使两个背景相互滚动，注意第二张背景图片初始位置在屏幕上方
+        #bg2 = Background("./images/background.png")
+        #bg2.rect.y = -bg2.rect.height
+        bg2 = Background(True)
+        self.back_group = pygame.sprite.Group(bg1, bg2)
         pass
 
     def start_game(self):
         print("游戏开始")
+
+        while True:
+            # 1.设置刷新帧率
+            self.clock.tick(CLOCK_TICK)
+            # 2.事件监听
+            self.__event_handler()
+            # 3.碰撞检测
+            self.__check_collide()
+            # 4.更新/绘制精灵组
+            self.__update_sprites()
+            # 5.更新显示
+            pygame.display.update()
+
+    def __event_handler(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("退出游戏。。")
+                pygame.quit()
+                exit()
+
+    def __check_collide(self):
+        pass
+
+    def __update_sprites(self):
+        # 1.更新背景精灵组并绘制到屏幕
+        self.back_group.update()
+        self.back_group.draw(self.screen)
+
+    @staticmethod
+    def __game_over():
+        """
+        游戏结束定义成静态方法
+        """
+        print("游戏结束。")
+        pygame.quit()
+        exit()
 
 
 if __name__ == "__main__":
--- 飞机大战/plane_sprites.py
+++ 飞机大战/plane_sprites.py
@@ -1,5 +1,9 @@
 import pygame
 
+# 定义常量
+SCREEN_RECT = pygame.Rect(0, 0, 400, 700)
+CLOCK_TICK = 60
+
 
 class GameSprite(pygame.sprite.Sprite):
     """
@@ -17,3 +21,21 @@ class GameSprite(pygame.sprite.Sprite):
     def update(self):
         # 在屏幕的垂直方向上移动
         self.rect.y += self.speed
+
+
+class Background(GameSprite):
+    """游戏背景精灵"""
+
+    def __init__(self, is_alt=False):
+        """is_alt参数用来区分第一张图片还是第二张图片，默认是第一张，如果为True则是第二张"""
+        super().__init__("./images/background.png")
+        if not is_alt:
+            self.rect.y = -self.rect.height
+
+    def update(self):
+        # 重写背景类的移动方法
+        # 1.调用父类的方法
+        super().update()
+        # 2.判断是否移出屏幕，如果移出屏幕，将图像设置到屏幕上方
+        if self.rect.y >= SCREEN_RECT.height:
+            self.rect.y = -self.rect.height
