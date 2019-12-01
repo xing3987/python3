7c234d810b9b7a2d58f8b0c02683cb60baa49477
chenxing <xing3987@163.com>
2019-12-02 00:35:17 +0800

    socket

---
+++ advance/_01_socket.py
@@ -0,0 +1,19 @@
+import socket
+
+
+def main():
+    # 1.创建一个udp套接字
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # 2.使用套接字收发数据socket.sendto("内容"(bytes),目标ip及端口)
+    #   发送内容有两种b"string","string".encode("utf-8")
+    while True:
+        datas = input("请输入要发送的数据：")
+        udp_socket.sendto(datas.encode("utf-8"), ("localhost", 8080))
+
+    # 3.关闭套接字
+    udp_socket.close()
+
+
+if __name__ == "__main__":
+    main()
